c62b3f7cd83bb7661629c261f3b2388a3d495551
David Erving <david.erving@hotmail.com>
2019-01-13 17:03:38 +1100

    fix

--- read_moisture.py
+++ read_moisture.py
@@ -15,12 +15,13 @@ Pin = 17
 #Define function to measure charge time
 def RC_Analog(Pin):
     fudgeFactor = 100
+    sleepTime = 0.1 # in seconds
     counter=0
     start_time = time.time()
     #Discharge capacitor
     GPIO.setup(Pin, GPIO.OUT)
     GPIO.output(Pin, GPIO.LOW)
-    time.sleep(0.1) #in seconds, suspends execution.
+    time.sleep(sleepTime) #in seconds, suspends execution.
     GPIO.setup(Pin, GPIO.IN)
 #Count loops until voltage across capacitor reads high on GPIO
     while (GPIO.input(Pin)==GPIO.LOW):
