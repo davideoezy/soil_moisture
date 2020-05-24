a6db50e37a328c816089bb2146b802c4f9a5cf04
david <david.erving@hotmail.com>
2020-05-24 14:56:05 +1000

    debug

--- utils/rc_check.py
+++ utils/rc_check.py
@@ -11,12 +11,12 @@ def RC_Analog(Pin):
     counter=0
     start_time = time.time()
     #Discharge capacitor
-    GPIO.setup(17, GPIO.OUT)
-    GPIO.output(17, GPIO.LOW)
+    GPIO.setup(Pin, GPIO.OUT)
+    GPIO.output(Pin, GPIO.LOW)
     time.sleep(0.1) #in seconds, suspends execution.
-    GPIO.setup(17, GPIO.IN)
+    GPIO.setup(Pin, GPIO.IN)
 #Count loops until voltage across capacitor reads high on GPIO
-    while (GPIO.input(17)==GPIO.LOW):
+    while (GPIO.input(Pin)==GPIO.LOW):
         counter=counter+1
     end_time = time.time()
     return end_time - start_time
@@ -31,19 +31,19 @@ while True:
     time_start = 0
     time_end = 0
     
-    print ts, reading  #print counts using GPIO4 and time
+    print(ts, reading)  #print counts using GPIO4 and time
     #file.write(str(ts) + " " + str(reading) + "\n") #write data to file
 
-    while (reading < 10.00):
-        time_start = time.time()
-        counter = counter + 1
-        if counter >= 50:
-            break
-    time_end = time.time()
-    if (counter >= 25 and (time_end - time_start) <= 60): # if you get 25 measurements that indicate dry soil in less than one minute, need to water
-        print('Not enough water for your plants to survive! Please water now.') #comment this out for testing
+    # while (reading < 10.00):
+    #     time_start = time.time()
+    #     counter = counter + 1
+    #     if counter >= 50:
+    #         break
+    # time_end = time.time()
+    # if (counter >= 25 and (time_end - time_start) <= 60): # if you get 25 measurements that indicate dry soil in less than one minute, need to water
+    #     print('Not enough water for your plants to survive! Please water now.') #comment this out for testing
 #    else:
  #     print('Your plants are safe and healthy, yay!')
 
 GPIO.cleanup()
-file.close()
+#file.close()
